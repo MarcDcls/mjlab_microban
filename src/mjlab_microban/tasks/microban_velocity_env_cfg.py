22caa9787358efdff35d246b3eff3f91a815cc08
MarcDcls <marc.duclusaud@gmail.com>
2026-06-09 10:30:59 +0200

    Adding IMU delay

--- src/mjlab_microban/tasks/microban_velocity_env_cfg.py
+++ src/mjlab_microban/tasks/microban_velocity_env_cfg.py
@@ -182,13 +182,19 @@ def make_microban_velocity_env_cfg(play: bool = False) -> ManagerBasedRlEnvCfg:
     cfg.observations["actor"].terms["base_ang_vel"] = deepcopy(
         cfg.observations["actor"].terms["base_ang_vel"]
     )
-    cfg.observations["actor"].terms["base_ang_vel"].delay_min_lag = 0
-    cfg.observations["actor"].terms["base_ang_vel"].delay_max_lag = 1
-    cfg.observations["actor"].terms["base_ang_vel"].delay_update_period = 64
-    cfg.observations["actor"].terms["projected_gravity"].delay_min_lag = 1
-    cfg.observations["actor"].terms["projected_gravity"].delay_max_lag = 1
-    cfg.observations["actor"].terms["projected_gravity"].delay_update_period = 64
+    # cfg.observations["actor"].terms["base_ang_vel"].delay_min_lag = 0
+    # cfg.observations["actor"].terms["base_ang_vel"].delay_max_lag = 1
+    # cfg.observations["actor"].terms["base_ang_vel"].delay_update_period = 64
+    # cfg.observations["actor"].terms["projected_gravity"].delay_min_lag = 1
+    # cfg.observations["actor"].terms["projected_gravity"].delay_max_lag = 1
+    # cfg.observations["actor"].terms["projected_gravity"].delay_update_period = 64
 
+    cfg.observations["actor"].terms["base_ang_vel"].delay_min_lag = 3
+    cfg.observations["actor"].terms["base_ang_vel"].delay_max_lag = 4
+    cfg.observations["actor"].terms["base_ang_vel"].delay_update_period = 64
+    cfg.observations["actor"].terms["projected_gravity"].delay_min_lag = 3
+    cfg.observations["actor"].terms["projected_gravity"].delay_max_lag = 5
+    cfg.observations["actor"].terms["projected_gravity"].delay_update_period = 64
     #---------------------------- Rewards ---------------------------
     cfg.rewards["track_linear_velocity"].params["std"] = np.sqrt(0.1)
     cfg.rewards["track_linear_velocity"].weight = 2.0
